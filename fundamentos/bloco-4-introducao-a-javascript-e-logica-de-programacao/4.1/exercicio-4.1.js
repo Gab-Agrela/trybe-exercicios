4da5d585c1f5c1ce8abc68ca083ea7a23234f9aa
Agrela <gabriel.agrela@hotmail.com>
2022-05-12 17:21:20 -0300

    Programa 5

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/4.1/exercicio-4.1.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/4.1/exercicio-4.1.js
@@ -1,5 +1,4 @@
 //Programa 1
-
 function calculadora (a,operador,b){
     let resultado = null
     if (operador === "+"){
@@ -14,31 +13,37 @@ function calculadora (a,operador,b){
         resultado = a % b
     
     }return typeof(resultado) === "number" ? resultado : console.log("Digite um operador válido!")
-}
-//console.log(calculadora(4,"*",2));
+}//console.log(calculadora(4,"*",2));
+
+//---------------------------------------------
 
 //Programa 2
-
 function qualMaior (a,b){
 
     return a>b ? a : b
-}
-//console.log(qualMaior (11,12));
+}//console.log(qualMaior (11,12));
 
+//---------------------------------------------
 
 //Programa 3
-
 function qualMaior2 (a,b,c){
 
     let maior = a>b ? a :b;
     
     return maior>c ? maior : c
-}
-//console.log(qualMaior2 (1,4,3));
+}//console.log(qualMaior2 (1,4,3));
+
+//---------------------------------------------
 
 //Programa 4
-
 function positivo(a){
     return a>0 ? "Positivo" : "Negativo"
-}
-console.log(positivo(3));
+}//console.log(positivo(3));
+
+//---------------------------------------------
+
+//Programa 5
+function triangulo(a,b,c){
+
+    return a>0 ? b>0 ? c>0 ? ((a+b+c) === 180 ? true : false) : "Digite angulos positivos" :"Digite angulos positivos":"Digite angulos positivos"
+}//console.log(triangulo (60,60,60))
