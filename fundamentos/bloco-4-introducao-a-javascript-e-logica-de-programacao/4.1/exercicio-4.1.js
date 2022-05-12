11260cfa0565416d3943253d654f83610285e8e2
Agrela <gabriel.agrela@hotmail.com>
2022-05-12 17:29:10 -0300

    Programa 6

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/4.1/exercicio-4.1.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/4.1/exercicio-4.1.js
@@ -47,3 +47,26 @@ function triangulo(a,b,c){
 
     return a>0 ? b>0 ? c>0 ? ((a+b+c) === 180 ? true : false) : "Digite angulos positivos" :"Digite angulos positivos":"Digite angulos positivos"
 }//console.log(triangulo (60,60,60))
+
+//Programa 6
+function xadrez (str){
+
+    let string = str.toLowerCase()
+
+    if (string === "peao"){
+        return "Só pra frente"
+    }else if (string === "torre"){
+        return "Horizontal e Vertical"
+    }else if (string === "cavalo"){
+        return "Se move em formato de L. Pulando uma casa"
+    }else if (string === "bispo"){
+        return "Somente nas Diagonais"
+    }else if (string === "rainha"){
+        return "Qualquer Direção"
+    }else if (string === "rei"){
+        return "Qualquer Direção"
+    }else {
+        return "Digite sem acentos ou digite uma peça válida."
+    }
+
+}console.log( xadrez ("peão"))
