75f17d934b6cb790e3f4e7b61dcf8657dac2a4b3
Agrela <gabriel.agrela@hotmail.com>
2022-05-12 17:10:36 -0300

    Programa 3

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/4.1/exercicio-4.1.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/4.1/exercicio-4.1.js
@@ -14,18 +14,24 @@ function calculadora (a,operador,b){
         resultado = a % b
     
     }return typeof(resultado) === "number" ? resultado : console.log("Digite um operador válido!")
-
-//console.log(calculadora(4,"*",2));
 }
-
+//console.log(calculadora(4,"*",2));
 
 //Programa 2
 
 function qualMaior (a,b){
 
     return a>b ? a : b
-
-//console.log(qualMaior (11,12));
 }
+//console.log(qualMaior (11,12));
 
 
+//Programa 3
+
+function qualMaior2 (a,b,c){
+
+    let maior = a>b ? a :b;
+    
+    return maior>c ? maior : c
+}
+//console.log(qualMaior2 (1,4,3));
