2d4c54729cd63d81e6eeea5a31a4eddec99ddc0a
Agrela <gabriel.agrela@hotmail.com>
2022-05-13 11:14:28 -0300

    Programa 4

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/4.2/exercicio-4.2.js
@@ -0,0 +1,30 @@
+let array = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+//Programa 1
+function programa1(array){
+    for(let a of array){
+        console.log(a)
+    }
+}//console.log(programa1(array))
+
+//--------------------------------------------------------------------
+
+//Programa 2
+function programa2(array){
+    let contador = 0
+    for(let i = 0; i<array.length; i+=1){
+        contador = contador + array[i]
+    }return contador
+}//console.log(programa2(array))
+
+//--------------------------------------------------------------------
+
+//Programa 3
+function programa3(array){
+    let soma = programa2(array)
+    let media = soma/array.length
+    return media > 20 ? "valor maior que 20" : "valor menor que 20"
+}//console.log(programa3(array))
+
+//--------------------------------------------------------------------
+
